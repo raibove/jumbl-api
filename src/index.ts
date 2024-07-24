a85b36d058a56859c707d068b0f7a3b4d2058730
Shweta kale <shwetakale144@gmail.com>
2024-07-24 22:10:16 +0530

    use api to generate topic list and clues

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,50 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
+export interface Env {
+	AI: Ai;
+}
+interface JumblRequest {
+	type: 'words';
+	topic: string;
+	numOfWords: string;
+	difficultyLevel: string;
+}
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request, env): Promise<Response> {
+		// const numOfWords = 10;
+		// const topic = 'Cooking';
+		// const difficultyLevel = 'Medium'
+
+		const canvaAppId = 'AAGKAGwRQxo';
+
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': `http://localhost:3000`,
+			'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+			'Access-Control-Max-Age': '86400',
+			'Access-Control-Allow-Headers': 'Content-Type',
+		};
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, { headers: corsHeaders });
+		}
+
+		if (request.method !== 'POST') {
+			return new Response('Please send a POST request', { status: 405, headers: corsHeaders });
+		}
+
+		const body: JumblRequest = await request.json();
+		const { type, topic, numOfWords, difficultyLevel} = body;
+
+		const response = (await env.AI.run("@hf/mistral/mistral-7b-instruct-v0.2", {
+			messages: [
+				{ role: "system", content: "You are a Professional Crossword Constructor who specialize in creating crosswords. Your task is to generate words for crossword and respond in [{answer: '', clue: ''}] format." },
+				{
+					role: "user",
+					content: `List ${numOfWords} words related to ${topic} with difficulty level ${difficultyLevel} for Crossword.`,
+				},
+			], stream: false
+		})) as { response: string };
+
+		return new Response(response.response, {
+			headers: { 'Content-Type': 'application/json', ...corsHeaders },
+		});
 	},
 } satisfies ExportedHandler<Env>;
